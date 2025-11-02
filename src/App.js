79406295e33b973ceb2ba026050988dd1e8ee8e5
saiHemanth <93516063+rebelsatya@users.noreply.github.com>
2025-11-02 15:14:34 +0530

    Update React version in App.js link

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 4.0 
+          Learn React 5.0 
         </a>
       </header>
     </div>
