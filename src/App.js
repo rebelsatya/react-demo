654abdcf49396414dc5f3f3577363cb102b5b7cb
hemanth-rebel <satyarebel1999@gmail.com>
2025-11-02 15:25:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 5.0 sai
+          Learn React 5.0 satya
         </a>
       </header>
     </div>
