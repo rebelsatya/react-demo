e927a5ecd6558b00e24b61a400da07f8822172c8
sai <satyahemanth.2255@gmail.com>
2025-11-09 10:42:27 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 5.0 Anandam Satya Sai hemanth
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+  return(
+    <>
+    <Navbar/>
+    </>
+  )
 }
 
 export default App;
+
+
+// function App() {
+
+//   const [fetchdata,setFetchData]=useState([])
+
+//   async function fetchData(){
+//     debugger;
+//     const response=await fetch("https://jsonplaceholder.typicode.com/todos");
+//     const data=await response.json()
+//     console.log(data)
+//     setFetchData(data);
+
+//   }
+
+//   useEffect(()=>{
+//     fetchData() 
+//   },[])
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React 5.0 
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import Button from '@mui/material/Button'
+import Checkbox from '@mui/material/Checkbox';
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import {Typography,Box, ListItem, ListItemButton, ListItemText, IconButton} from '@mui/material'
+import Container from '@mui/material/Container'
+import CoffeeIcon from '@mui/icons-material/Coffee';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+
+const label = { slotProps: { input: { 'aria-label': 'Checkbox demo' } } };
+
+
+const Navbar = () => {
+    const [drawerOpen,setDrawerOpen]=useState(false);
+    const theme=useTheme();
+
+    const isMobile=useMediaQuery(theme.breakpoints.down("md"));
+
+    const toggleDrawer=(open)=>{
+        setDrawerOpen(open)
+    }
+
+    const drawerLinks=[
+        {text:"Home",link:"#home"},
+        {text:"Coffee",link:"#coffee"}
+    ]
+
+  return (
+    <div>
+        <AppBar position="sticky" color="primary">
+            <Container>
+                <Toolbar>
+                    <CoffeeIcon/>
+                  <Typography variant="h6" sx={{ flexGrow: 1, fontFamily: '"Eagle Lake", serif' }}>
+  Coffee Shop
+</Typography>
+
+                    {isMobile && (<IconButton color="inherit" onClick={()=>toggleDrawer(true)}>
+                        <MenuIcon/>
+                    </IconButton>)}
+
+                    {!isMobile && (<><Button color="inherit" href="#home">
+                          Home
+                      </Button><Button color="inherit" href="#cofee">
+                              Coffee
+                          </Button></>)}
+                </Toolbar>
+            </Container>
+        
+        </AppBar>
+
+        <Drawer  anchor="right" open={drawerOpen} onClose={() => toggleDrawer(false)}
+  >
+            <Box sx={{width:200}} role="presentation" onClick={() => toggleDrawer(false)}
+         >
+                <List>
+                    {drawerLinks.map((item,index)=>(
+                        <ListItem key={index} disablePadding>
+                            <ListItemButton  component="a" href={item.link}
+                            onClick={() =>toggleDrawer(false)}
+                            aria-label={`Navigate to ${item.text}`}>
+                                <ListItemText primary={item.text}/>
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+
+                </List>
+
+            </Box>
+          
+        </Drawer>
+        </div>
+  )
+}
+
+export default Navbar
